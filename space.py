38cda4a9e91814812326250fbac80e8a5c0ea0e8
Przemo8922 <przemyslawsalak@gmail.com>
2017-04-25 17:18:47 +0200

    myspace1

--- space.py
+++ space.py
@@ -22,7 +22,7 @@ def create_board(width,height):
 def print_board(board):
 	for row in board:
 		for char in row:
-			print(char, end='')
+			print(char, end = '')
 		print()
 
 
@@ -32,39 +32,39 @@ def insert_player(board, width, height):
 
 
 def getch():
-    import sys, tty, termios
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
+	import sys, tty, termios
+	fd = sys.stdin.fileno()
+	old_settings = termios.tcgetattr(fd)
+	try:
+		tty.setraw(sys.stdin.fileno())
+		ch = sys.stdin.read(1)
+	finally:
+		termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+	return ch
 
 
 def move(x_pos,y_pos,char):
-    if char == 'a' and x_pos > 1:
-        x_pos -= 1
-    elif char == 'd' and x_pos < 58:
-        x_pos += 1
-    elif char == 'w' and y_pos > 1:
-        y_pos -= 1
-    elif char == 's' and y_pos < 18:
-        y_pos += 1
-    return x_pos, y_pos        
+	if char == 'a' and x_pos > 1:
+		x_pos -= 1
+	elif char == 'd' and x_pos < 58:
+		x_pos += 1
+	elif char == 'w' and y_pos > 1:
+		y_pos -= 1
+	elif char == 's' and y_pos < 18:
+		y_pos += 1
+	return x_pos, y_pos
 
 
 def main():
-    x_pos = 15
-    y_pos = 15
-    char = ''
-    while char != 'p':
-        x_pos, y_pos = move(x_pos, y_pos, char)
-        os.system("clear")
-        board = create_board(60,20)
-        board = insert_player(board, x_pos, y_pos)
-        print_board(board)
-        char = getch()
+	x_pos = 15
+	y_pos = 15
+	char = ''
+	while char != 'p':
+		x_pos, y_pos = move(x_pos, y_pos, char)
+		os.system("clear")
+		board = create_board(60,20)
+		board = insert_player(board, x_pos, y_pos)
+		print_board(board)
+		char = getch()
 
 main()
